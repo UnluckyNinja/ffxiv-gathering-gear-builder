7bca7618ea83d811b73fee252b0ef7d0bf91c02f
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2022-09-22 23:54:25 +0800

    fix: fix materias not reactive

--- composables/materia.ts
+++ composables/materia.ts
@@ -2,7 +2,7 @@ import type { MaybeRef } from '@vueuse/core'
 import type { Materia } from '~~/data/materias'
 
 export function useMaterias(initial?: MaybeRef<(Materia | null)[]>) {
-  const materias = shallowRef(initial ?? [null, null, null, null, null])
+  const materias = ref(initial ?? [null, null, null, null, null])
 
   return {
     materias,
